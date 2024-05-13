c00a8846172c5b73de335e99b55bbadc335f8f03
Vinita Patil <vinipatil2325@gmail.com>
2024-05-13 16:16:18 +0530

    first commit

--- src/Header.js
+++ src/Header.js
@@ -16,7 +16,7 @@ const Header = () => {
   return (
     <header className="bg-blue-700 text-white py-4 px-4 fixed top-0 left-0 right-0 z-50">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-xl md:text-3xl font-bold cursor-pointer" onClick={() => scrollToSection('about')}>Ms. Vinita Vikram Patil</h1>
+        <h1 className="text-xl md:text-3xl font-bold cursor-pointer ml-5" onClick={() => scrollToSection('about')}>Ms. Vinita Vikram Patil</h1>
         <nav>
           <ul className="flex space-x-4 md:space-x-8 mr-10">
             <li><a href="#" className="text-base md:text-lg font-medium hover:text-black" onClick={(e) => handleNavClick('about', e)}>About</a></li>
